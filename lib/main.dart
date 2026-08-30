e69163b40ba13804cbd88771803f7815c93ae6ef
parshw1 <parasj7765r@gmail.com>
2026-08-30 20:20:45 +0530

    day1

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,16 @@
 import 'package:flutter/material.dart';
-
-void main(){
+import 'package:currency_converter/pages/homepage.dart';
+void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My First Flutter App'),
-        ),
-        body: const Center(
-          child: Text('Welcome Paras!',
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 22, 134, 26)),
-          ),
-        ),
-      )
+      home: CounterPage(),
     );
   }
 }
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,37 @@
+import "package:flutter/material.dart";
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter App'),
+      ),
+      body: Center(
+        child: Text(
+          '$counter',
+          style: const TextStyle(
+            fontSize: 40,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
